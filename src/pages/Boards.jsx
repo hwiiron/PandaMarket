3064f677708b7da83ebf3a773f640f9268ecfdd7
Hwichul Shin <shinhwiiron@gmail.com>
2024-10-21 23:37:24 +0900

    자유게시판 페이지 추가

---
+++ src/pages/Boards.jsx
@@ -0,0 +1,14 @@
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
+
+const Boards = () => {
+  return (
+    <>
+      <Header />
+      <div>자유게시판</div>
+      <Footer />
+    </>
+  );
+};
+
+export default Boards;
