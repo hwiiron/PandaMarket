1080b654172a0c24fdffd94eec61b3616be162e3
Hwichul Shin <shinhwiiron@gmail.com>
2024-11-08 20:36:26 +0900

    메인을 제외한 페이지에서만 Navigation 노출

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -4,14 +4,17 @@ import Logo from "./Logo";
 import Navigation from "./Navigation";
 import ProfileImg from "./ProfileImg";
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <StyledHeader>
       <StyledHeaderInner>
         <Logo />
 
-        <Navigation />
+        {location.pathname !== "/" ? <Navigation /> : ""}
 
         <Button type={"LOGIN"} text={"로그인"} to={"/login"} />
         {/* <ProfileImg /> */}
