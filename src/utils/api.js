2c97ef8516049548adc3df767098c66f3a139315
Hwichul Shin <shinhwiiron@gmail.com>
2024-11-08 20:37:14 +0900

    해상도에 따라 상품  갯수 노출을 다르게 하기 위해 pageSize prop 추가

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,16 @@
 const BASE_URL = "https://panda-market-api.vercel.app/products";
 
-const getBestProducts = async () => {
+const getBestProducts = async (pageSize) => {
   const response = await fetch(
-    `${BASE_URL}?page=1&pageSize=4&orderBy=favorite`
+    `${BASE_URL}?page=1&pageSize=${pageSize}&orderBy=favorite`
   );
   const data = response.json();
   return data;
 };
 
-const getAllProducts = async ({ page, orderBy }) => {
+const getAllProducts = async ({ page, orderBy, pageSize }) => {
   const response = await fetch(
-    `${BASE_URL}?page=${page}&pageSize=10&orderBy=${orderBy}`
+    `${BASE_URL}?page=${page}&pageSize=${pageSize}&orderBy=${orderBy}`
   );
   const data = await response.json();
   return data;
